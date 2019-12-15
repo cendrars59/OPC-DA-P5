b41d30a595002e12bd068a9425c138eea64046ef
Cyril <cyr.leroux.dev@moinslequart.net>
2019-12-15 21:56:09 +0100

    Fixing cat parms files

--- Utils/Params/feedParams.py
+++ Utils/Params/feedParams.py
@@ -6,5 +6,13 @@ params = {
 
   'type': 'store',
   'table': 'store',
-  'request':openfoodfacts.facets.get_stores(),}  
+  'request':openfoodfacts.facets.get_stores(),
+  },
+
+  'category': {
+
+    'type': 'category',
+    'table': 'category',
+    'request':openfoodfacts.facets.get_categories(),
+  }  
 }
--- Utils/datafeed.py
+++ Utils/datafeed.py
@@ -25,28 +25,5 @@ def feed(type,request,table):
 
 def feedApp():
     feed(params["store"]["type"],params["store"]["request"],params["store"]["table"])
+    feed(params["category"]["type"],params["category"]["request"],params["category"]["table"])
 
-<<<<<<< HEAD
-
-
-def FeedCategories():
-    queryForCheck = ("SELECT COUNT(*) FROM Category WHERE code=%s")
-    queryForInserting = ("INSERT INTO category (code,name,url) VALUES(%s,%s,%s)")
-    insertCount = 0
-    categories = openfoodfacts.facets.get_categories()
-    conn = connect()
-    cursor = conn.cursor()
-
-    for category in categories: 
-        cursor.execute(queryForInserting, (category['id'],category['name'],category['url']))
-        insertCount += 1
-        print('category inserted')
-        if insertCount == 1000:
-            conn.commit()
-            insertCount = 0
-    cursor.close()
-    print("Categories feed has been done!")
-
-
-=======
->>>>>>> MD_Cat
