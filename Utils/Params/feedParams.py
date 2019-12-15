1d36d08e7efae5c189a4e6afaaad5b7698c6d28b
Cyril <cyr.leroux.dev@moinslequart.net>
2019-12-15 21:42:24 +0100

    Removing old version of feed cat +  adding category params

---
+++ Utils/Params/feedParams.py
@@ -0,0 +1,17 @@
+# -*- coding: Utf-8 -*
+import openfoodfacts
+
+params = {
+  'store' : {
+
+    'type': 'store',
+    'table': 'store',
+    'request':openfoodfacts.facets.get_stores(),
+  },
+
+  'category' : {
+    'type': 'category',
+    'table': 'category',
+    'request':openfoodfacts.facets.get_categories(),
+  }  
+}
